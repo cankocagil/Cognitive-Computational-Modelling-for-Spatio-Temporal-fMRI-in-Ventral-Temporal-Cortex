e3b09a2ef21b4ac41b92fabb16f311483fcbd8e7
Can Kocagil <53329652+cankocagil@users.noreply.github.com>
2021-05-03 00:24:45 +0300

    Added utils

--- utils/savers.py
+++ utils/savers.py
@@ -2,12 +2,12 @@ import pickle
 import numpy as np
 
 def save_obj(obj:object, path:str = None) -> None:
-    with open(name + '.pkl', 'wb') as f:
+    with open(path + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
         
 def load_obj(path:str = None) -> object:
-    with open(name + '.pkl', 'rb') as f:
+    with open(path + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 
